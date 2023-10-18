f2523cc8033f594d16fe475440bb02ab40a5ede5
Hensonkk <keyshawn2015@gmail.com>
2023-10-17 22:34:05 -0500

    Working on module 15 hw. Almost finsihed. Going to have to turn in late.

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -15,16 +15,50 @@ let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.g
 // Get our GeoJSON data
 d3.json(link).then(function(data){
     // function to style
-        function style(feature) {
+        function styles(feature) {
             return {
                 opacity: .5,
-                fillColor: chooseColor(feature.geometry.coordinates[2]),
+                fillColor: getColor(feature.geometry.coordinates[2]),
                 fillOpcaity: .5, 
                 radius: radius(feature.properties.mag),
-                weight: 1
+                weight: 1,
+                stroke: true
             };
         }
+
     // function to get radius from magnitude
-        function radius(mag)
+        function radius(magnitude) {
+            if (magnitude === 0) {
+                return 1;
+            }
+            return (magnitude) * 4;
+        }
+
+    // function to get color from depth
+        function getColor(depth) {
+            if (depth < 10) return "#99d600";
+            else if (depth < 25) return "#d6af00";
+            else if (depth < 50) return "#d66f00";
+            else if (depth < 75) return "#d64400";
+            else if (depth < 100) return "#d63200";
+            else return "#d60000";
+        }
+
+    // geoJSON layer
+
+    L.geoJson(data, {
+        // circle Marker
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng),
+
+        // Popup for each layer
+        function onEachFeature(feature, layer){
+            layer.bindPopup(`<h1>Location is ${feature.properties.place}</h1> <h2>Magnitude is ${feature.properties.mag} <br> Depth is ${feature.geometry.coordinates[2]} </h2>`)
+        }},
+
+    // style for circleMarkers
+        style: styles
+
+    }).addTo(myMap);
      
-})
+});
