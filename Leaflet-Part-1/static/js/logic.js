d2c3633748a3f2f4f47dc0d076ee4d5098c48646
Hensonkk <keyshawn2015@gmail.com>
2023-10-17 23:58:31 -0500

    Finished the challenge. Can create the legend but can not get the colors to populate next to the legend bar. Will try and fix.

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -17,9 +17,9 @@ d3.json(link).then(function(data){
     // function to style
         function styles(feature) {
             return {
-                opacity: .5,
+                opacity: 1,
                 fillColor: getColor(feature.geometry.coordinates[2]),
-                fillOpcaity: .5, 
+                fillOpcaity: 1, 
                 radius: radius(feature.properties.mag),
                 weight: 1,
                 stroke: true
@@ -45,20 +45,37 @@ d3.json(link).then(function(data){
         }
 
     // geoJSON layer
-
-    L.geoJson(data, {
+    L.geoJSON(data.features, {
         // circle Marker
         pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng),
+            return L.circleMarker(latlng)},
 
         // Popup for each layer
-        function onEachFeature(feature, layer){
+        onEachFeature: function onEachFeature(feature, layer){
             layer.bindPopup(`<h1>Location is ${feature.properties.place}</h1> <h2>Magnitude is ${feature.properties.mag} <br> Depth is ${feature.geometry.coordinates[2]} </h2>`)
-        }},
+        },
 
     // style for circleMarkers
         style: styles
 
     }).addTo(myMap);
      
+    // Create legend
+    let legend = L.control({position: 'topright'});
+
+    legend.onAdd = function () {
+
+        let div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 25, 50, 75, 100];
+
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(myMap);
 });
